19cd2ae97daaf3a7523beff49f9b6ef87f195137
Victor Caleb Izquierdo Zuloaga <calebizqui@gmail.com>
2021-10-25 09:17:18 -0500

    Configuracion del puerti

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const cors = require('cors')
 // ...
 const userCtrl = require('./controller/userController')
 
-const PORT = 3001 || process.env.PORT
+const PORT = process.env.PORT || 3001
 
 // ...
 app.use(cors())
